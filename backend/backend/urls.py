305c1acd8f3a7847da07f155d9cd18bc26833cd5
Erik <erik.kundt@gmail.com>
2013-05-21 23:07:42 +0200

    created api app

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'backend.views.home', name='home'),
+    url(r'^$', 'backend.views.home', name='home'),
     # url(r'^backend/', include('backend.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
