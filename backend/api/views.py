892d4ff8e3e5e9cb57d3557efecb1e00b856a275
Erik <erik.kundt@gmail.com>
2013-05-22 09:02:17 +0200

    started coding the login process

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,1 +1,22 @@
 # Create your views here.
+from django.http import HttpResponse, authenticate, login
+
+def index(request):
+    return HttpResponse("SWP-UCD Eule API 1.0")
+
+def register(request):
+    return HttpResponse("register")
+
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+        	login(request, user)
+            # Redirect to a success page.
+            
+        else:
+            # Return a 'disabled account' error message
+    else:
+        # Return an 'invalid login' error message.
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,11 +2,13 @@ from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from api import views
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'backend.views.home', name='home'),
+    # url(r'^$', 'backend.views.home', name='home'),
     # url(r'^backend/', include('backend.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
@@ -14,4 +16,9 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    # home
+    url(r'^$', views.index, name='index'),
+    url(r'^register', views.register, name='register'),
+    url(r'^login', views.login, name='login')
 )
