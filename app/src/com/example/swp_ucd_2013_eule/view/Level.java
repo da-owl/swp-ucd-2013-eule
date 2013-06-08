eb11e201f77563a85232e1c20433db8033ce4b8d
da-mkay <manuel@wenns-um-email-geht.de>
2013-06-08 10:48:14 +0200

    Level-View: using dps

--- app/src/com/example/swp_ucd_2013_eule/view/Level.java
+++ app/src/com/example/swp_ucd_2013_eule/view/Level.java
@@ -1,21 +1,26 @@
 package com.example.swp_ucd_2013_eule.view;
 
-import com.example.swp_ucd_2013_eule.R;
-
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.Point;
 import android.graphics.Paint.Align;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.View;
+
+import com.example.swp_ucd_2013_eule.R;
 
 public class Level extends View {
 	private Bitmap mLevelBitmap = BitmapFactory.decodeResource(getResources(),
 			R.drawable.ico_level);
 
+	private int mSize;
+	private Rect mSrcRect = new Rect(0, 0, 48, 48);
+	private Rect mDstRect = new Rect();
 	private Paint mTextPaint;
 	private Point mTextPos;
 
@@ -37,9 +42,13 @@ public class Level extends View {
 		mTextPaint.setColor(0xFFffffff);
 		mTextPaint.setTextAlign(Align.CENTER);
 		mTextPaint.setTextSize(20);
+	}
 
-		mTextPos = new Point(24,
-				(int) (24 - ((mTextPaint.descent() + mTextPaint.ascent()) / 2)));
+	private void updateDimensions() {
+		mTextPos = new Point(
+				mSize / 2,
+				(int) (mSize / 2 - ((mTextPaint.descent() + mTextPaint.ascent()) / 2)));
+		mDstRect.set(0, 0, mSize, mSize);
 	}
 
 	public void setLevel(int level) {
@@ -47,16 +56,23 @@ public class Level extends View {
 		invalidate();
 	}
 
+	protected int dpToPx(int val) {
+		return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+				val, getResources().getDisplayMetrics());
+	}
+
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
-		canvas.drawBitmap(mLevelBitmap, 0, 0, null);
+		canvas.drawBitmap(mLevelBitmap, mSrcRect, mDstRect, null);
 		canvas.drawText(mLevel, mTextPos.x, mTextPos.y, mTextPaint);
 	}
 
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-		setMeasuredDimension(48, 48);
+		mSize = dpToPx(32);
+		updateDimensions();
+		setMeasuredDimension(mSize, mSize);
 	}
 }
