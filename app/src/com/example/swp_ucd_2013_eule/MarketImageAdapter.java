716acd045fb791a156330193c9bef2773d7dbc2a
Samantha Rouse <samjrouse@live.co.uk>
2013-06-11 14:34:28 +0200

    Clothing in market with correct layout
    Grid layout with no of points required for purchase, rather than a list

---
+++ app/src/com/example/swp_ucd_2013_eule/MarketImageAdapter.java
@@ -0,0 +1,75 @@
+package com.example.swp_ucd_2013_eule;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+
+public class MarketImageAdapter extends BaseAdapter{
+
+	private Context mContext;
+    public static final int ACTIVITY_CREATE = 10;
+
+	public  MarketImageAdapter(Context c) {
+	    mContext = c;
+	}
+
+	public int getCount() {
+	    return mThumbIds.length;
+	}
+
+	public Object getItem(int position) {
+	    return null;
+	}
+
+	public long getItemId(int position) {
+	    return 0;
+	}
+
+
+
+
+	        @Override
+	        public View getView(int position, View convertView, ViewGroup parent) {
+	            // TODO Auto-generated method stub
+	            View v;
+	            if(convertView==null){
+	                LayoutInflater li = LayoutInflater.from(mContext);
+	                v = li.inflate(R.layout.fragment_market_item, null);
+	                TextView tv = (TextView)v.findViewById(R.id.icon_text);
+	                tv.setText(mNoPoints[position]);
+	                ImageView iv = (ImageView)v.findViewById(R.id.icon_image);
+	                iv.setImageResource(mThumbIds[position]);
+	 
+	            }
+	            else
+	            {
+	                v = convertView;
+	            }
+	            return v;
+	    }
+
+    
+
+
+	private Integer[] mThumbIds = {
+	        R.drawable.item_clothes, R.drawable.item_clothes,
+	        R.drawable.item_clothes, R.drawable.item_clothes,
+	        R.drawable.item_clothes, R.drawable.item_clothes,
+	        R.drawable.item_clothes, R.drawable.item_clothes,
+	        R.drawable.item_clothes, R.drawable.item_clothes,
+	};
+	
+	private String[] mNoPoints = {
+			"5 Points", "8 Points",
+			"15 Points", "17 Points",
+			"25 Points", "50 Points",
+			"45 Points", "100 Points",
+			"500 Points", "520 Points",
+	};
+}
