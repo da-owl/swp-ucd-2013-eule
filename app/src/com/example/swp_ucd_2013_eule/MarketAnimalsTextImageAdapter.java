9cbb1005e688f0d3660ab2624e2795d3c9312346
Samantha Rouse <samjrouse@live.co.uk>
2013-06-13 10:21:47 +0200

    Market now uses ForestItems
    Still an example list used though, nothing dynamic

--- app/src/com/example/swp_ucd_2013_eule/MarketAnimalsTextImageAdapter.java
+++ app/src/com/example/swp_ucd_2013_eule/MarketAnimalsTextImageAdapter.java
@@ -1,6 +1,10 @@
 package com.example.swp_ucd_2013_eule;
 
+import com.example.swp_ucd_2013_eule.data.ForestItem;
+import com.example.swp_ucd_2013_eule.data.ForestItem.ForestItemType;
+
 import android.content.Context;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,13 +15,44 @@ import android.widget.TextView;
 public class MarketAnimalsTextImageAdapter extends BaseAdapter{
 
 	private Context mContext;
+	private ForestItem[] mItems;
 
 	public MarketAnimalsTextImageAdapter(Context c) {
 		mContext = c;
+		mItems = makeExampleAnimals();
+	}
+
+	private ForestItem[] makeExampleAnimals() {
+		ForestItem bird1 = new ForestItem( ForestItemType.STANDARD,
+				BitmapFactory.decodeResource(mContext.getResources(),
+				R.drawable.item_animals), "Bird1", "This is bird1. It is Brown", 2);
+		ForestItem bird2 = new ForestItem( ForestItemType.STANDARD,
+				BitmapFactory.decodeResource(mContext.getResources(),
+				R.drawable.item_animals), "Bird2", "This is bird2. It is also Brown", 1);
+		ForestItem bird3 = new ForestItem( ForestItemType.STANDARD,
+				BitmapFactory.decodeResource(mContext.getResources(),
+				R.drawable.item_animals), "Bird3", "This is bird1. Brown", 3);
+		ForestItem bird4 = new ForestItem( ForestItemType.STANDARD,
+				BitmapFactory.decodeResource(mContext.getResources(),
+				R.drawable.item_animals), "Bird4", "This is bird4. It is Brown", 5);
+		ForestItem bird5 = new ForestItem( ForestItemType.STANDARD,
+				BitmapFactory.decodeResource(mContext.getResources(),
+				R.drawable.item_animals), "Bird5", "This is bird5. It is Brown", 2);
+		ForestItem bird6 = new ForestItem( ForestItemType.STANDARD,
+				BitmapFactory.decodeResource(mContext.getResources(),
+				R.drawable.item_animals), "Bird6", "This is bird6. It is the last bird", 2);
+		bird1.setPrice(5);
+		bird2.setPrice(15);
+		bird3.setPrice(40);
+		bird4.setPrice(100);
+		bird5.setPrice(115);
+		bird6.setPrice(205);
+		ForestItem[] exampleAnimals = new ForestItem[] {bird1, bird2, bird3, bird4, bird5, bird6};
+		return exampleAnimals;
 	}
 
 	public int getCount() {
-		return mThumbIds.length;
+		return mItems.length;
 	}
 
 	public Object getItem(int position) {
@@ -36,9 +71,9 @@ public class MarketAnimalsTextImageAdapter extends BaseAdapter{
 			LayoutInflater li = LayoutInflater.from(mContext);
 			v = li.inflate(R.layout.fragment_market_item, null);
 			TextView tv = (TextView) v.findViewById(R.id.icon_text);
-			tv.setText(mNoPoints[position]);
+			tv.setText(mItems[position].getPrice() + " Points");
 			ImageView iv = (ImageView) v.findViewById(R.id.icon_image);
-			iv.setImageResource(mThumbIds[position]);
+			iv.setImageBitmap(mItems[position].getImage());
 
 		} else {
 			v = convertView;
@@ -46,18 +81,5 @@ public class MarketAnimalsTextImageAdapter extends BaseAdapter{
 		return v;
 	}
 
-	private Integer[] mThumbIds = { R.drawable.item_animals,
-			R.drawable.item_animals, R.drawable.item_animals,
-			R.drawable.item_animals, R.drawable.item_animals,
-			R.drawable.item_animals, R.drawable.item_animals,
-			R.drawable.item_animals, R.drawable.item_animals,
-			R.drawable.item_animals, R.drawable.item_animals,
-			R.drawable.item_animals, R.drawable.item_animals,
-			R.drawable.item_animals, R.drawable.item_animals,
-			R.drawable.item_animals,};
-
-	private String[] mNoPoints = { "5 Points", "8 Points", "15 Points",
-			"17 Points", "25 Points", "50 Points", "45 Points", "100 Points",
-			"500 Points", "520 Points", "598 Points", "650 Points", 
-			"650 Points", "780 Points", "999 Points", "1000 Points"};
+	
 }
--- app/src/com/example/swp_ucd_2013_eule/MarketPlantsTextImageAdapter.java
+++ app/src/com/example/swp_ucd_2013_eule/MarketPlantsTextImageAdapter.java
@@ -1,6 +1,10 @@
 package com.example.swp_ucd_2013_eule;
 
+import com.example.swp_ucd_2013_eule.data.ForestItem;
+import com.example.swp_ucd_2013_eule.data.ForestItem.ForestItemType;
+
 import android.content.Context;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,13 +15,73 @@ import android.widget.TextView;
 public class MarketPlantsTextImageAdapter extends BaseAdapter{
 
 	private Context mContext;
+	private ForestItem[] mItems;
 
 	public  MarketPlantsTextImageAdapter(Context c) {
 	    mContext = c;
+	    mItems = makeExamplePlants();
+	}
+
+	private ForestItem[] makeExamplePlants() {
+		ForestItem fir1 = new ForestItem( ForestItemType.STANDARD,
+				BitmapFactory.decodeResource(mContext.getResources(),
+				R.drawable.item_fir), "Fir", "This is a fir. It is a tree", 2);
+		ForestItem fir2 = new ForestItem( ForestItemType.STANDARD,
+				BitmapFactory.decodeResource(mContext.getResources(),
+				R.drawable.item_fir), "Fir", "This is a fir. It is a tree", 2);
+		ForestItem fir3 = new ForestItem( ForestItemType.STANDARD,
+				BitmapFactory.decodeResource(mContext.getResources(),
+				R.drawable.item_fir), "Fir", "This is a fir. It is a tree", 2);
+		ForestItem fir4 = new ForestItem( ForestItemType.STANDARD,
+				BitmapFactory.decodeResource(mContext.getResources(),
+				R.drawable.item_fir), "Fir", "This is a fir. It is a tree", 2);
+		ForestItem fir5 = new ForestItem( ForestItemType.STANDARD,
+				BitmapFactory.decodeResource(mContext.getResources(),
+				R.drawable.item_fir), "Fir", "This is a fir. It is a tree", 2);
+		ForestItem bush1 = new ForestItem( ForestItemType.STANDARD,
+				BitmapFactory.decodeResource(mContext.getResources(),
+				R.drawable.item_bush), "Bush", "This is a bush. It is green", 2);
+		ForestItem bush2 = new ForestItem( ForestItemType.STANDARD,
+				BitmapFactory.decodeResource(mContext.getResources(),
+				R.drawable.item_bush), "Bush", "This is a bush. It is green", 2);
+		ForestItem bush3 = new ForestItem( ForestItemType.STANDARD,
+				BitmapFactory.decodeResource(mContext.getResources(),
+				R.drawable.item_bush), "Bush", "This is a bush. It is green", 2);
+		ForestItem bush4 = new ForestItem( ForestItemType.STANDARD,
+				BitmapFactory.decodeResource(mContext.getResources(),
+				R.drawable.item_bush), "Bush", "This is a bush. It is green", 2);
+		ForestItem tulip1 = new ForestItem( ForestItemType.STANDARD,
+				BitmapFactory.decodeResource(mContext.getResources(),
+				R.drawable.item_plants), "Tulip", "This is a flower.", 2);
+		ForestItem tulip2 = new ForestItem( ForestItemType.STANDARD,
+				BitmapFactory.decodeResource(mContext.getResources(),
+				R.drawable.item_plants), "Tulip", "This is a pink flower.", 2);
+		ForestItem tulip3 = new ForestItem( ForestItemType.STANDARD,
+				BitmapFactory.decodeResource(mContext.getResources(),
+				R.drawable.item_plants), "Tulip", "This is a flower.", 2);
+		ForestItem tulip4 = new ForestItem( ForestItemType.STANDARD,
+				BitmapFactory.decodeResource(mContext.getResources(),
+				R.drawable.item_plants), "Tulip", "This is a tulip. It is pink.", 2);
+		fir1.setPrice(50);
+		fir2.setPrice(90);
+		fir3.setPrice(150);
+		fir4.setPrice(200);
+		fir5.setPrice(250);
+		bush1.setPrice(290);
+		bush2.setPrice(310);
+		bush3.setPrice(450);
+		bush4.setPrice(450);
+		tulip1.setPrice(490);
+		tulip2.setPrice(500);
+		tulip3.setPrice(510);
+		tulip4.setPrice(520);
+		ForestItem[] examplePlants = new ForestItem[] {fir1, fir2, fir3, fir4, fir5, 
+				bush1, bush2, bush3, bush4, tulip1, tulip2, tulip3, tulip4};
+		return examplePlants;
 	}
 
 	public int getCount() {
-	    return mThumbIds.length;
+	    return mItems.length;
 	}
 
 	public Object getItem(int position) {
@@ -33,15 +97,14 @@ public class MarketPlantsTextImageAdapter extends BaseAdapter{
 
 	        @Override
 	        public View getView(int position, View convertView, ViewGroup parent) {
-	            // TODO Auto-generated method stub
 	            View v;
 	            if(convertView==null){
 	                LayoutInflater li = LayoutInflater.from(mContext);
 	                v = li.inflate(R.layout.fragment_market_item, null);
 	                TextView tv = (TextView)v.findViewById(R.id.icon_text);
-	                tv.setText(mNoPoints[position]);
+	                tv.setText(mItems[position].getPrice() + " Points");
 	                ImageView iv = (ImageView)v.findViewById(R.id.icon_image);
-	                iv.setImageResource(mThumbIds[position]);
+	                iv.setImageBitmap(mItems[position].getImage());
 	 
 	            }
 	            else
