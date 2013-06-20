dd4a36953cad8d8c070f1ea0638e3fffde8343ab
Samantha Rouse <samjrouse@live.co.uk>
2013-06-20 11:44:18 +0100

    Market view contains points and level required
    I didn't know how to write it in German, if someone could just change
    the text, or tell me what to change it to... that'd be grand :)

--- app/src/com/example/swp_ucd_2013_eule/MarketForestItemAdapter.java
+++ app/src/com/example/swp_ucd_2013_eule/MarketForestItemAdapter.java
@@ -69,7 +69,12 @@ public class MarketForestItemAdapter extends BaseAdapter {
 			v = li.inflate(R.layout.fragment_market_item, null);
 			TextView tv = (TextView) v.findViewById(R.id.icon_text);
 			tv.setText(item.getPrice() + " Tropfen");
-			if(item.getPrice() > 80 || item.getLevel() > 17){
+			if(item.getPrice() > 80){
+				tv.setTextColor(Color.RED);
+			}
+			tv = (TextView) v.findViewById(R.id.icon_text2);
+			tv.setText(item.getLevel() + " 	Forest size");
+			if(item.getLevel() > 17){
 				tv.setTextColor(Color.RED);
 			}
 			ImageView iv = (ImageView) v.findViewById(R.id.icon_image);
