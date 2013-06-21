6a0a6cc334e61bc1c5fbf98e952f1d44fe8be470
Marc <simonsm@zedat.fu-berlin.de>
2013-06-21 18:15:07 +0200

    fixed use of deprecated method in settings

--- app/src/com/example/swp_ucd_2013_eule/SettingsActivity.java
+++ app/src/com/example/swp_ucd_2013_eule/SettingsActivity.java
@@ -1,15 +1,16 @@
 package com.example.swp_ucd_2013_eule;
 
-import com.example.swp_ucd_2013_eule.R;
+import android.app.Activity;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		addPreferencesFromResource(R.layout.activity_settings);
-	}
-
+        // Display the fragment as the main content.
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
 }
---
+++ app/src/com/example/swp_ucd_2013_eule/SettingsFragment.java
@@ -0,0 +1,16 @@
+package com.example.swp_ucd_2013_eule;
+
+import com.example.swp_ucd_2013_eule.R;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+
+public class SettingsFragment extends PreferenceFragment {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.layout.activity_settings);
+	}
+
+}
